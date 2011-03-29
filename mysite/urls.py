1a543359b460e84de54546716cdf71f933091696
Andreas Neustifter <andreas.neustifter@gmail.com>
2011-03-29 21:29:09 +0200

    Moved all polls specific urls into app and include them from top level.

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,11 +4,8 @@ from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('polls.views',
-    (r'^polls/$', 'index'),
-    (r'^polls/(?P<poll_id>\d+)/$', 'detail'),
-    (r'^polls/(?P<poll_id>\d+)/results/$', 'results'),
-    (r'^polls/(?P<poll_id>\d+)/vote/$', 'vote'),
+urlpatterns = patterns('',
+    (r'^polls/', include('polls.urls')),
 )
 
 urlpatterns += patterns('',
