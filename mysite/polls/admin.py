be8fb100def6ef8650aa0d944f4a5b5087ca732b
Andreas Neustifter <andreas.neustifter@gmail.com>
2011-03-29 20:41:47 +0200

    Added hierarchical date browsing to poll list.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -10,6 +10,7 @@ class PollAdmin(admin.ModelAdmin):
     list_display = ('question', 'pub_date', 'was_published_today')
     list_filter = ['pub_date']
     search_fields = ['question']
+    date_hierarchy = 'pub_date'
 
     fieldsets = [
         (None,               {'fields': ['question']}),
