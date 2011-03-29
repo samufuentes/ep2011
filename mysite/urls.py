ea2e72152fb5282d4c010a42922f07cc380bc605
Andreas Neustifter <andreas.neustifter@gmail.com>
2011-03-29 20:49:46 +0200

    Added urls for poll app.

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,9 +5,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^mysite/', include('mysite.foo.urls')),
+    (r'^polls/$', 'polls.views.index'),
+    (r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
+    (r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
+    (r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
