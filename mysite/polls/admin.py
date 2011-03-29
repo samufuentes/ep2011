46299e0f346b2e008b5b4f49e7f5f71f08bc599f
Andreas Neustifter <andreas.neustifter@gmail.com>
2011-03-29 20:35:22 +0200

    Removed separate choice page and included choices into the poll page.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,14 +1,16 @@
 from polls.models import Poll
+from polls.models import Choice
 from django.contrib import admin
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 class PollAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Poll, PollAdmin)
-
-from polls.models import Choice
-
-admin.site.register(Choice)
