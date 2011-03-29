4a7dad9a5392de3aaafc4b77e060e123c936fbaf
Andreas Neustifter <andreas.neustifter@gmail.com>
2011-03-29 21:09:45 +0200

    Use get_object_or_404 to fetch object and return 404 if none was found.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,16 +1,12 @@
 from polls.models import Poll
-from django.http import Http404
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
     return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
 
 def detail(request, poll_id):
-    try:
-        p = Poll.objects.get(pk=poll_id)
-    except Poll.DoesNotExist:
-        raise Http404
+    p = get_object_or_404(Poll, pk=poll_id)
     return render_to_response('polls/detail.html', {'poll': p})
 
 def results(request, poll_id):
