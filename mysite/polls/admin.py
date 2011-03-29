0d46046381beb8f1455753cf89585e5034cd577d
Andreas Neustifter <andreas.neustifter@gmail.com>
2011-03-29 20:31:37 +0200

    Further refined poll admin page.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 class PollAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
 
 admin.site.register(Poll, PollAdmin)
