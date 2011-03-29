00f6bb0897f3988d1b958f44d1070ab757ef609b
Andreas Neustifter <andreas.neustifter@gmail.com>
2011-03-29 21:07:53 +0200

    Create proper view for single poll, properly raise 404 if poll not found.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,5 @@
 from polls.models import Poll
+from django.http import Http404
 from django.shortcuts import render_to_response
 
 def index(request):
@@ -6,11 +7,14 @@ def index(request):
     return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    try:
+        p = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render_to_response('polls/detail.html', {'poll': p})
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
 
 def vote(request, poll_id):
     return HttpResponse("You're voting on poll %s." % poll_id)
-
