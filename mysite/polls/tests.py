c2bd75c5dc3689613f2c34ca158a9bae2e0bf21e
Samuel Fuentes <info@samuelfuentes.com>
2011-06-20 00:42:25 +0200

    Testing views

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -6,6 +6,7 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.test import Client
 from polls.models import Poll, Choice
 
 
@@ -16,7 +17,7 @@ class SimpleTest(TestCase):
         """
         self.assertEqual(1 + 1, 2)
 
-class PollsTest(TestCase):
+class PollsDBTest(TestCase):
     fixtures = ["test_data.json"]
 
     def test_get_poll(self):
@@ -26,3 +27,11 @@ class PollsTest(TestCase):
     def test_count_choices(self):
         self.assertEqual(Choice.objects.all().count(), 3)
 
+class PollsViewsTest(TestCase):
+    fixtures = ["test_data.json"]
+
+    def test_vote(self):
+        c = Client()
+        response = c.post("/polls/1/vote/", {'choice': '1'})
+        self.assertEqual(response.status.code, 302)
+        self.assertEqual(Choice.objects.get(pk=1).votes, 3)
